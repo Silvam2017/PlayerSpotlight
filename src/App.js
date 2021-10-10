b8bd5449e132216374b516a18912c02cce1d6173
Silvam2017 <msilva@aggienetwork.com>
2021-10-10 18:12:34 -0400

    added support for reactstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import Table from './Components/MaterialTable.js';
 
 function App() {
   return (
     <div className="App">
-      <header>
-
-      </header>
       <div>
-        <Table id="table1"></Table>
-        <Table id="table2"></Table>
+        <nav>
+          <ul>
+            <li>
+              <a href='home'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='contact'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <header background-image='../assets/img/swift1.jpg'>
+          <h2>
+            D'Andre Swift
+          </h2>
+          <p>
+            Running Back
+          </p>
+        </header>
+      </div>
+      <div>
+
       </div>
     </div>
   );
